4978bed4c255326efab13a555b792e1a91f22759
karthik.iyer <karthik.iyer@slalom.com>
2020-07-06 11:44:43 -0500

    Added Component Scanners

--- Pluralsight_Spring_Fundamentals/conference-Java/src/main/java/com/pluralsight/com/pluralsight/service/SpeakerServiceImpl.java
+++ Pluralsight_Spring_Fundamentals/conference-Java/src/main/java/com/pluralsight/com/pluralsight/service/SpeakerServiceImpl.java
@@ -3,15 +3,24 @@ package com.pluralsight.com.pluralsight.service;
 import com.pluralsight.model.Speaker;
 import com.pluralsight.repository.HibernateSpeakerRepositoryImpl;
 import com.pluralsight.repository.SpeakerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service("speakerService")
 public class SpeakerServiceImpl implements SpeakerService {
 
     private SpeakerRepository speakerRepository;
 
+    public SpeakerServiceImpl(){
+        System.out.println("In the Speaker Service Impl no args Constructor");
+    }
+
+    @Autowired
     public SpeakerServiceImpl(SpeakerRepository speakerRepository){
         this.speakerRepository = speakerRepository;
+        System.out.println("In the Speaker Service Impl speakerRepository Constructor");
     }
 
     @Override
@@ -19,7 +28,9 @@ public class SpeakerServiceImpl implements SpeakerService {
         return speakerRepository.findAll();
     }
 
+
     public void setSpeakerRepository(SpeakerRepository speakerRepository) {
         this.speakerRepository = speakerRepository;
+        System.out.println("In the Speaker Service Impl in the setter");
     }
 }
