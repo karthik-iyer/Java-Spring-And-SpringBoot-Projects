dc7fdecfc8c27a3a33bc174c90ea0392ecf82ba9
karthik.iyer <karthik.iyer@slalom.com>
2020-07-06 14:42:05 -0500

    Added Xml Spring Config Demo

---
+++ Pluralsight_Spring_Fundamentals/conference-XML/src/main/java/com/pluralsight/service/SpeakerServiceImpl.java
@@ -0,0 +1,21 @@
+package com.pluralsight.service;
+
+import com.pluralsight.model.Speaker;
+import com.pluralsight.repository.SpeakerRepository;
+
+import java.util.List;
+
+public class SpeakerServiceImpl implements SpeakerService {
+
+    private SpeakerRepository speakerRepository;
+
+    @Override
+    public List<Speaker> findAll(){
+        return speakerRepository.findAll();
+    }
+
+
+    public void setSpeakerRepository(SpeakerRepository speakerRepository) {
+        this.speakerRepository = speakerRepository;
+    }
+}
